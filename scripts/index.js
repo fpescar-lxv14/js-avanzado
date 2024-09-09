eff398ff967ac217bd4af8d21585616d82744803
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-08 22:40:55 -0300

    ejemplos tipos de datos y propagacion de eventos

--- scripts/index.js
+++ scripts/index.js
@@ -23,18 +23,33 @@ const
 
 let textoCopia = texto, objetoCopia = objeto;
 
-console.log(texto, textoCopia, objeto, objetoCopia,)
+console.log(texto, objeto, textoCopia, objetoCopia,)
 
 texto = "c215714n";
 objeto.nombre = "c215714n";
 
-console.log(texto, textoCopia, objeto, objetoCopia,)
+console.log(texto, objeto, textoCopia, objetoCopia,)
 
 /** Propagacion de Eventos
  *  fase 1 (Capturing): Javascript recorre el arbol DOM desde el objeto WINDOW hasta el elemento afectado por el evento
  *  fase 2 (Target) : Javascript encuentra el nodo afectado/disparado el evento llevado a cabo.
  *  fase 3 (Bubbling): Javascript recorre en sentido inverso el arbol DOM, desde el nodo objetivo, hasta el objeto WINDOW
  */
+const toggle = (selector, className="active") => 
+    document.querySelector(selector)
+    .classList.toggle(className)
+
 document.addEventListener('click', (event) => {
-    console.log(event.target)
+    event.stopPropagation();
+    const { id, tagName } = event.target
+    switch(tagName){
+        case "INPUT":
+        case "BUTTON":
+            event.preventDefault();
+            if (id === "menu") toggle("#menuList")            
+        break;
+        case "A":
+            console.log("no soy un boton")
+        break;
+    }
 })
