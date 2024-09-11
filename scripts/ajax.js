f4ddcd69b8209f98116c007f5e69215c15271ee1
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-11 17:24:56 -0300

    ejemplos xhr javascript asincrono

---
+++ scripts/ajax.js
@@ -0,0 +1,26 @@
+function ajax(url, callback){
+    // 0: solicitud sin cargar
+    const xhr = new XMLHttpRequest()
+    // 1: carga de solicitud
+    xhr.open("GET", url)
+    // 3-4: procesamiento y finalizacion
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === 4){
+            if (xhr.status < 400) 
+            callback(xhr.responseText)
+        } 
+    } )
+    // 2: envio de solicitud
+    xhr.send()
+}
+const main = document.querySelector('main');
+const linkButtons = document.querySelectorAll('[data-link]')
+
+linkButtons.forEach(btn => {
+    btn.onclick = (e) => {
+        const link = e.target.getAttribute('data-link');
+        const url = `/assets/${link}.txt`
+        const response = (data) => main.innerHTML=data
+        ajax(url,response)
+    }
+})
