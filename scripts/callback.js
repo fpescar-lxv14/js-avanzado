e82ec4df31f7c5846547266198809ec234979b0f
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-13 16:01:11 -0300

    comparacion callback y promise

--- scripts/callback.js
+++ scripts/callback.js
@@ -1,5 +1,21 @@
+const ajax = ({url, method = "GET", callback}) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open(method,url)
+    xhr.addEventListener('readystatechange', () => {
+        if(xhr.readyState === 4) {
+            if (xhr.status === 200){
+                const data = JSON.parse(xhr.response)
+                callback(data)
+            }
+            else callback([{
+                error: 404,
+                message: "no se ha encontrado la pagina"
+            }])
+        }
+    })
+    xhr.send()
+}
 // callback Hell | Pyramid of Doom
-
 ajax({
     url: `${server}/${posts}`,
     callback: (posts) => {
---
+++ scripts/promise.js
@@ -0,0 +1,24 @@
+const ajax = ({url, method = "GET"}) => {
+    const promise = new Promise(resolve, reject)
+    const xhr = new XMLHttpRequest()
+    xhr.open(method,url)
+    xhr.addEventListener('readystatechange', () => {
+        if(xhr.readyState === 4) {
+            if (xhr.status === 200){
+                resolve(xhr.response)
+            }
+            else reject([{
+                error: 404,
+                message: "no se ha encontrado la pagina"
+            }])
+        }
+    })
+    xhr.send()
+        return promise
+    }
+
+ajax({url: `${server}/posts`})
+.then(response => JSON.parse(response))
+.then(data => data.map(d => ajax(`${server}/comments?postId=${d.id}`)))
+.then(comments => comments.map(comment => console.log(comment)))
+.catch(error => console.error(error.message))
