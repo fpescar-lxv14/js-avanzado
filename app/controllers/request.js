f00b45266502a1de99f9f9dc33c1ed2f8ac1b4a7
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-16 15:37:10 -0300

    agregar identificacion de datos en solicitud

--- app/controllers/request.js
+++ app/controllers/request.js
@@ -1,7 +1,18 @@
-async function Request(url, config = {}){
+export default async function Request(url, config = {}){
     try{
+        let data;
         const response = await fetch(url, config)
-        const data = await response.json()
+        const contentType = response.headers.get('Content-Type')
+        if(/application\/text/.test(contentType)){
+            data = await response.text()
+        }
+        else if (/image|video|audio/.test(contentType)){
+            data = await response.blob()
+            data = URL.createObjectURL(data)
+        }
+        else {
+            data = await response.json()
+        }
         return data
     }
     catch(error) {
