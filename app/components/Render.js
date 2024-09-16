f04794c233d45f9c7eb41b470367b0c3e642c046
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-16 16:06:37 -0300

    renderizado de amigos

--- app/components/Render.js
+++ app/components/Render.js
@@ -1,12 +1,17 @@
 import { root, main } from "../models/index.js";
+import Friend from "./Friend.js";
 
 export default function Render(data){
-    main.innerHTML ='';
+    main.innerHTML = ''
+    const section = document.createElement('section')
+    section.setAttribute('class', 'container col-md-9 col-xl-6 d-grid gap-2')
+    section.innerHTML = "<h2>Amigos</h2>"
     if (data.length){
-
+        data.forEach(item => {
+            const renderData = Friend(item)
+            section.innerHTML += renderData
+        })
     }
-    else {
-        
-    }
+    main.appendChild(section)
     root.appendChild(main)
 }
