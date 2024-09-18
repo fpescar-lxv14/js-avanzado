0fcc9edec40199cdb6588479a5f1a473971f5d3f
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-18 15:37:41 -0300

    eliminar evento click de componente navegacion

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,6 +1,4 @@
 import { links, toggle, root } from '../models/index.js'
-import Request from '../controllers/request.js'
-import Render from './Render.js'
 import { Button } from './Button.js'
 import { MapLinks } from './Link.js'
 
@@ -10,15 +8,7 @@ export default function Nav(App){
     innerHTML:`
         <a class="navbar-brand">${App ?? "JS"}</a>
         ${Button(toggle)}
-        <ul class="navbar-nav navbar-collapse collapse">${MapLinks(links)}</ul>`,
-    onclick: async (e) => {
-        e.preventDefault()
-        if (e.target.tagName === "A") {
-            const link = e.target.href.split("#")[1]
-            const data = await Request('https://jsonplaceholder.typicode.com/'+link)
-            Render(data)
-        }
-    }
+        <ul class="navbar-nav navbar-collapse collapse">${MapLinks(links)}</ul>`
     })
     root.appendChild(nav);
 }
