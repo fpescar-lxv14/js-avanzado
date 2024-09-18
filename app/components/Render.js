85db59df4b253a5ed627b06e45bb6500584a20d2
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-18 15:36:37 -0300

    importar componentes y mapeo de rutas

--- app/components/Render.js
+++ app/components/Render.js
@@ -1,16 +1,40 @@
 import { root, main } from "../models/index.js";
+import Album from "./Album.js"
 import Friend from "./Friend.js";
+import Photo from "./Photo.js";
+import Post from "./Post.js";
 
-export default function Render(data){
+const ArrayPaths = {
+    posts: Post,
+    users: Friend,
+    albums: Album,
+    photos: Photo,
+}
+const ObjPaths = {
+
+}
+const whatsThis = (obj, word) => {
+    let thisIsIt;
+    Object.keys(obj).map(k => { 
+        if(word.includes(k)) thisIsIt = obj[k]
+    })
+    return thisIsIt;
+}
+export default function Render(data, link){
     main.innerHTML = ''
+    let Component;
     const section = document.createElement('section')
-    section.setAttribute('class', 'container col-md-9 col-xl-6 d-grid gap-2')
-    section.innerHTML = "<h2>Amigos</h2>"
+    section.setAttribute('class', 'container row col-md-9 col-xl-6 mx-auto')
     if (data.length){
-        data.forEach(item => {
-            const renderData = Friend(item)
+        Component = whatsThis(ArrayPaths, link)
+        data.forEach(async item => {
+            const renderData = await Component(item)
             section.innerHTML += renderData
         })
+    }
+    else{
+        Component = whatsThis(ObjPaths, link)
+        section.innerHTML = Component(data)
     }
     main.appendChild(section)
     root.appendChild(main)
