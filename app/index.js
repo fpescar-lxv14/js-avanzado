8e2813cfcb607f1897f7aa20e0fadf586235401f
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-18 15:38:21 -0300

    implementar controlador de rutas

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
+import router from "./controllers/router.js"
 import Nav from "./components/Nav.js"
 
 // IIFE (Inmediatly Invoked Function Expression)
 (() => document.addEventListener('DOMContentLoaded', () => {
     Nav("SPA")
+    document.addEventListener('click', (e) => router(e))
 }))()
